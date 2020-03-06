4b7aa7723e310568dd27239c522558135aa1c656
rigox <rigoc1994@gmail.com>
2020-03-05 23:56:09 -0500

    changed the project to simple person upload

---
+++ model/Person.js
@@ -0,0 +1,25 @@
+const mongoose =  require("mongoose")
+const Schema =   mongoose.Schema
+
+
+const personSchema =  new Schema({
+       name:{
+            type:String,
+            required:[true,'please add name']
+       },
+       age:{
+            type:String,
+            required:[true,'pleaese add your age']
+       },
+       sex:{
+            type:String
+       },
+       photo_url:{
+            type:String
+       }
+        
+       
+})
+
+
+module.exports =  mongoose.model('Persons',personSchema)
