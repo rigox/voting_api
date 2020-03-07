73c74188671b0659cec0b64d2e7de4d534019d5a
rigox <rigoc1994@gmail.com>
2020-03-06 23:47:26 -0500

    added logic to the add photo function

--- controllers/person.js
+++ controllers/person.js
@@ -1,7 +1,7 @@
-const Topic =  require("../model/Topic")
+const Person =  require("../model/Person")
 const asyncHandler =  require("../middleware/async")
-
-
+const path   =  require("path")
+const aws =  require("aws-sdk")
 // @desc get all topics
 //@route  GET /api/v1/Topic
 //@access  Public
@@ -34,4 +34,55 @@ exports.createPerson   = asyncHandler( async (req,res,next)=>{
 //@POST /api/v1/person/id
 //@access Public
 
-exports.addPhoto  = asyncHandler(async (req,res,next)=>{});
+exports.addPhoto  = asyncHandler(async (req,res,next)=>{
+   
+     const person =   await Person.findById(req.params.id)
+
+     if(!person){
+               return "person not found"
+     }
+
+     if(!req.files){
+           return "please upload a file"
+     }
+
+      const file =   req.files.photo;
+
+    // make sure the  file is a  photo
+
+     if(!file.mimetype.startsWith('image')){
+          return "please  upload and image"
+     }
+
+     // check file size
+
+     if(file.size > process.env.MAX_FILE_UPLOAD){
+             return  "please upload a smaller file"
+     }
+
+     //create the file name
+
+     file.name  =  `Photo_${person.id}${path.parse(file.name).ext}`;
+    
+
+     const s3  =  new aws.s3()
+
+     const params = {
+              Bucket:'rigo002',
+              Key:file.name,
+              Body:file,
+              ACL:'public-read'
+     }
+
+     s3.putObject(params,function(err,data){
+            if(err){throw err}
+            res.status(200).json({
+                   success:true,
+                   data:'file uploaded'
+            })
+     });
+
+
+       
+
+});
